34d08fa544945dbe7fcedc2910ca7a54a38dac49
Ronaldo Souza Chaves <ronaldosouzachaves@outlook.com>
2023-09-23 17:38:54 -0300

    feat(ap10): adiciona exercício 1/1.3

--- script.sql
+++ script.sql
@@ -169,6 +169,148 @@ $$
 -- Entrada: O arquivo de entrada contém dois valores inteiros.
 -- Saída: O programa deve imprimir um valor inteiro. Este valor é a soma dos valores ímpares que estão entre os valores fornecidos na entrada que deverá caber em um inteiro.
 
+-- LOOP
+
+DO
+$$
+DECLARE
+	contador INT := 1;
+	soma INT := 0;
+	x INT := valor_aleatorio_entre(20, 50);
+	y INT := valor_aleatorio_entre(20, 50);
+	a INT := 0;
+	b INT := 0;
+BEGIN
+	IF x < y THEN
+		a := x;
+		b := y;
+	ELSE
+		a := y;
+		b := x;
+	END IF;
+	
+	RAISE NOTICE 'X: %', a;
+	RAISE NOTICE 'Y: %', b;
+		
+	LOOP
+		IF (a + contador) % 2 <> 0 THEN
+			soma := soma + (a + contador);
+			RAISE NOTICE 'soma: %', soma;
+		END IF;
+		contador := contador + 1;
+		EXIT WHEN contador >= (b - a);
+	END LOOP;
+	RAISE NOTICE 'soma final: %', soma;
+END;
+$$
+
+-- WHILE
+
+DO
+$$
+DECLARE
+	contador INT := 1;
+	soma INT := 0;
+	x INT := valor_aleatorio_entre(20, 50);
+	y INT := valor_aleatorio_entre(20, 50);
+	a INT := 0;
+	b INT := 0;
+BEGIN
+	IF x < y THEN
+		a := x;
+		b := y;
+	ELSE
+		a := y;
+		b := x;
+	END IF;
+	
+	RAISE NOTICE 'X: %', a;
+	RAISE NOTICE 'Y: %', b;
+		
+	WHILE contador < (b - a) LOOP
+		IF (a + contador) % 2 <> 0 THEN
+			soma := soma + (a + contador);
+			RAISE NOTICE 'soma: %', soma;
+		END IF;
+		contador := contador + 1;
+	END LOOP;
+	RAISE NOTICE 'soma final: %', soma;
+END;
+$$
+
+-- FOR
+
+DO
+$$
+DECLARE
+	contador INT := 1;
+	soma INT := 0;
+	x INT := valor_aleatorio_entre(20, 50);
+	y INT := valor_aleatorio_entre(20, 50);
+	a INT := 0;
+	b INT := 0;
+BEGIN
+	IF x < y THEN
+		a := x;
+		b := y;
+	ELSE
+		a := y;
+		b := x;
+	END IF;
+	
+	RAISE NOTICE 'X: %', a;
+	RAISE NOTICE 'Y: %', b;
+		
+	FOR contador IN 1..(b - a - 1) LOOP
+        IF (a + contador) % 2 <> 0 THEN
+            soma := soma + (a + contador);
+            RAISE NOTICE 'soma: %', soma;
+        END IF;
+    END LOOP;
+	RAISE NOTICE 'soma final: %', soma;
+END;
+$$
+
+-- FOREACH
+
+DO
+$$
+DECLARE
+    soma INT := 0;
+    x INT := valor_aleatorio_entre(20, 50);
+    y INT := valor_aleatorio_entre(20, 50);
+    a INT := 0;
+    b INT := 0;
+    contador INT := 1;
+    numerosImpares INT[] := ARRAY[]::INT[];
+	i INT;
+BEGIN
+    IF x < y THEN
+        a := x;
+        b := y;
+    ELSE
+        a := y;
+        b := x;
+    END IF;
+
+    RAISE NOTICE 'X: %', a;
+    RAISE NOTICE 'Y: %', b;
+
+    FOR i IN a+1..b-1 LOOP
+        IF i % 2 <> 0 THEN
+            numerosImpares := array_append(numerosImpares, i);
+        END IF;
+    END LOOP;
+
+    FOREACH i IN ARRAY numerosImpares LOOP
+        soma := soma + i;
+        RAISE NOTICE 'soma: %', soma;
+    END LOOP;
+
+    RAISE NOTICE 'soma final: %', soma;
+END;
+$$;
+
 -- 1.1/4 Leia um conjunto não determinado de pares de valores M e N (parar quando algum dos valores for menor ou igual a zero). Para cada par lido, mostre a sequência do menor até o maior e a soma dos inteiros consecutivos entre eles (incluindo o N e M).
 -- Entrada? O arquivo de entrada contém um número não determinado de valores M e N. A última linha de entrada vai conter um número nulo ou negativo.
 -- Saída: Para cada dupla de valores, imprima a sequência do menor até o maior e a soma deles, conforme exemplo abaixo.
